c12dd15dd40ff7c3f64dce9254abdf6d0a25b1c5
iniyaelaya2010 <iniyaelaya2010@gmail.com>
2026-08-28 09:05:46 -0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "delivery-tracker-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./login.js",
+  "./carrier.js",
+  "./employer.js",
+  "./map.js",
+  "./next.js",
+  "./timer.js",
+  "./ai.js",
+  "./voice.js",
+  "./image.js",
+  "./data.js",
+  "./history.js",
+  "./stats.js",
+  "./settings.js",
+  "./db.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
